4728bb51ff27abac649b57b8a82e0f28c1663989
Nahuel López <nahuioh.1891@gmail.com>
2024-03-24 11:18:14 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from 'react';
-import CryptoList from './CryptoList';
+import CryptoList from './pages/CryptoList';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el archivo CSS de Bootstrap
 import './App.css';
 
